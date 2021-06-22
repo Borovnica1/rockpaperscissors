ac188832317eab4c2c48efbf2dbea830c82657f0
Borovnica1 <petkovic.czv1@gmail.com>
2021-06-22 23:20:32 +0200

    dom updated

--- script.js
+++ script.js
@@ -1,44 +1,68 @@
+let playerWonRounds = 0;
+let computerWonRounds = 0;
+const buttons = document.querySelectorAll('.buttons__option');
+
+
 function computerPlay() {
   const moves = ['paper', 'rock', 'scissors'];
   return moves[Math.floor(Math.random()*3)];
 }
 
-function playRound(playerChoice, computerChoice) {
-  playerChoice = playerChoice.toLowerCase();
-  computerChoice = computerChoice.toLowerCase();
+function playRound(e) {
+  playerChoice = this.getAttribute('data-choice')
+  computerChoice = computerPlay();
   
   if (playerChoice === computerChoice) {
-    return [2, 'Draw!']
+    updateScore([2, 'Draw!'])
   } else if (playerChoice === 'paper' && computerChoice === 'rock') {
-    return [0, 'You Win! Paper beats Rock']
+    updateScore([0, 'You Win! Paper beats Rock'])
   } else if (playerChoice === 'rock' && computerChoice === 'paper') {
-    return [1, 'You Lose! Paper beats Rock']
+    updateScore([1, 'You Lose! Paper beats Rock'])
   } else if (playerChoice === 'paper' && computerChoice === 'scissors') {
-    return [1, 'You Lose! Scissors beats Paper']
+    updateScore([1, 'You Lose! Scissors beats Paper'])
   } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
-    return [0, 'You Win! Scissors beats paper']
+    updateScore([0, 'You Win! Scissors beats paper'])
   } else if (playerChoice === 'scissors' && computerChoice === 'rock') {
-    return [1, 'You Lose! Rock beats Scissors']
+    updateScore([1, 'You Lose! Rock beats Scissors'])
   } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
-    return [0, 'You Win! Rock beats Scissors']
+    updateScore([0, 'You Win! Rock beats Scissors']);
   }
 }
 
-function game() {
-  let playerWonRounds = 0;
-  let computerWonRounds = 0;
-  for (let i = 0; i < 5; i++) {
-    const playerChoice = prompt(`Type: \'paper\' or \'rock\' or \'scissors\'! Won round: You-${playerWonRounds} Computer-${computerWonRounds}`);
-    
-    let winnerArr = playRound(playerChoice, computerPlay());
-    
-    console.log(winnerArr[0], winnerArr[1])
-    if (winnerArr[0] === 2) {
-      playerWonRounds++;
-      computerWonRounds++;
-    } else if (winnerArr[0] === 1) computerWonRounds++;
-    else playerWonRounds++;
-  }
-  console.log(`The winner is ${playerWonRounds > computerWonRounds ? 'You' : 'Computer'}`);
+function gameFinished(winner) {
+  document.querySelector('.result').style.display = 'flex';
+  document.querySelector('.result__text').textContent = `The winner is ${winner}`;
+  document.querySelector('.buttons__options').style.display = 'none';
 }
-game()
+
+function updateScore(winnerArr) {    
+  document.querySelector('.result-text').textContent = winnerArr[1];
+  if (winnerArr[0] === 2) {
+    playerWonRounds++;
+    computerWonRounds++;
+  } else if (winnerArr[0] === 1) computerWonRounds++;
+  else playerWonRounds++;
+
+  document.querySelector('.score__player').firstElementChild.textContent = playerWonRounds;
+  document.querySelector('.score__computer').firstElementChild.textContent = computerWonRounds;
+
+  if (playerWonRounds === 5 && computerWonRounds === 5) {
+    gameFinished('Draw');
+  } else if (playerWonRounds === 5) {
+    gameFinished('Player');
+  } else if (computerWonRounds === 5) {
+    gameFinished('Computer');
+  }
+}
+
+function reset() {
+  document.querySelector('.buttons__options').style.display = 'block';
+  document.querySelector('.result').style.display = 'none';
+  playerWonRounds = 0;
+  computerWonRounds = 0;
+  document.querySelector('.score__player').firstElementChild.textContent = playerWonRounds;
+  document.querySelector('.score__computer').firstElementChild.textContent = computerWonRounds;
+}
+
+buttons.forEach(button => button.addEventListener('click', playRound));
+document.querySelector('.btn-reset').addEventListener('click', reset);
